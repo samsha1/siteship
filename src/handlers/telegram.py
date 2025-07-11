6a68b59a46333b05faab70363581644ceb3115e5
samrat <samratshakya5@gmail.com>
2025-07-11 14:56:24 +0545

    debugging sendMessage logger added

--- src/handlers/telegram.py
+++ src/handlers/telegram.py
@@ -3,6 +3,7 @@ import httpx
 from src.common.config import settings
 from src.common.logger import get_logger
 logger = get_logger(__name__)
+import requests
 
 TELEGRAM_BOT_TOKEN = settings.TELEGRAM_BOT_TOKEN
 
@@ -10,6 +11,8 @@ TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
 
 async def send_message(chat_id: int, text: str):
     logger.info("Link: %s ---- chat_id: %s - text: %s", f"{TELEGRAM_API_URL}/sendMessage",chat_id, text)
+    response = await requests.request("GET", f"https://api.telegram.org/bot{TELEGRAM_API_URL}/getWebhookInfo", headers={}, data={},timeout=60)
+    logger.info(response.text)
     async with httpx.AsyncClient() as client:
         response = await client.post(
             f"{TELEGRAM_API_URL}/sendMessage",
